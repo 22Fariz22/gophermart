1f75f058de84a013a92262e8494ba50117be62af
22fariz22 <fariz08@gmail.com>
2023-02-22 22:25:09 +0300

    try fix config

--- internal/config/config.go
+++ internal/config/config.go
@@ -7,15 +7,15 @@ import (
 )
 
 type Config struct {
-	runAddress           string
-	databaseURI          string
-	accrualSystemAddress string
+	runAddress           string `env:"RUN_ADDRESS"` //envDefault:":8080"
+	databaseURI          string `env:"DATABASE_URI"`
+	accrualSystemAddress string `env:"ACCRUAL_SYSTEM_ADDRESS"`
 }
 
 func NewConfig() (*Config, error) {
 	cfg := &Config{}
 
-	flag.StringVar(&cfg.runAddress, "a", "http://localhost:8080", "server address")
+	flag.StringVar(&cfg.runAddress, "a", "localhost:8080", "server address")
 	flag.StringVar(&cfg.databaseURI, "d", "", "database address")        //postgres://postgres:55555@127.0.0.1:5432/gophermart
 	flag.StringVar(&cfg.accrualSystemAddress, "r", "", "accrual system") // http://127.0.0.1:8080
 
