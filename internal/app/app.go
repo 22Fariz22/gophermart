d11966f57a0d8a62053c44ad8a4d5c384b4cb998
22fariz22 <fariz08@gmail.com>
2023-02-22 21:59:42 +0300

    try fix config

--- internal/app/app.go
+++ internal/app/app.go
@@ -88,7 +88,7 @@ func (a *App) Run() error {
 
 	// HTTP Server
 	a.httpServer = &http.Server{
-		Addr:           viper.GetString("d"), //":" + "8080", //8080
+		Addr:           viper.GetString("a"), //":" + "8080", //8080
 		Handler:        router,
 		ReadTimeout:    10 * time.Second,
 		WriteTimeout:   10 * time.Second,
