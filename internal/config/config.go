e241658a8eed40133d00f1fe98ae01b3ff6a382e
22fariz22 <fariz08@gmail.com>
2023-02-22 23:56:22 +0300

    try fix config

--- internal/config/config.go
+++ internal/config/config.go
@@ -7,17 +7,17 @@ import (
 )
 
 type Config struct {
-	runAddress           string `env:"RUN_ADDRESS"` //envDefault:":8080"
-	databaseURI          string `env:"DATABASE_URI"`
-	accrualSystemAddress string `env:"ACCRUAL_SYSTEM_ADDRESS"`
+	RunAddress           string `env:"RUN_ADDRESS"` //envDefault:":8080"
+	DatabaseURI          string `env:"DATABASE_URI"`
+	AccrualSystemAddress string `env:"ACCRUAL_SYSTEM_ADDRESS"`
 }
 
 func NewConfig() *Config {
 	cfg := &Config{}
 
-	flag.StringVar(&cfg.runAddress, "a", "localhost:8080", "server address")
-	flag.StringVar(&cfg.databaseURI, "d", "", "database address")        //postgres://postgres:55555@127.0.0.1:5432/gophermart
-	flag.StringVar(&cfg.accrualSystemAddress, "r", "", "accrual system") // http://127.0.0.1:8080
+	flag.StringVar(&cfg.RunAddress, "a", "localhost:8080", "server address")
+	flag.StringVar(&cfg.DatabaseURI, "d", "", "database address")        //postgres://postgres:55555@127.0.0.1:5432/gophermart
+	flag.StringVar(&cfg.AccrualSystemAddress, "r", "", "accrual system") // http://127.0.0.1:8080
 
 	pflag.CommandLine.AddGoFlagSet(flag.CommandLine)
 	pflag.Parse()
