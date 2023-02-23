a28087fe8af28242718dd4e9c8cc5d61af9c541a
22fariz22 <fariz08@gmail.com>
2023-02-23 13:20:31 +0300

    try fix config

--- cmd/gophermart/main.go
+++ cmd/gophermart/main.go
@@ -1,18 +1,20 @@
 package main
 
 import (
-	"fmt"
 	"github.com/22Fariz22/gophermart/internal/app"
 	"github.com/22Fariz22/gophermart/internal/config"
+	"log"
 	"os"
 )
 
 func main() {
 	cfg := config.NewConfig()
 
-	fmt.Println("len", len(os.Args))
+	log.Println("cfg from main: ", cfg)
+
+	log.Println("len", len(os.Args))
 	for _, arg := range os.Args[1:] {
-		fmt.Println("arg: ", arg)
+		log.Println("arg: ", arg)
 	}
 	app := app.NewApp(cfg)
 
